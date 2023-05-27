72a0b524e5c007ab6a6617cbea86c4dfac647ea9
Julymeltdown <quintuplets2000@gmail.com>
2023-05-27 22:28:49 +0900

    flask에서 사용자 해시태그별 점수 가져오기 성공

--- app.py
+++ app.py
@@ -1,59 +1,52 @@
-from flask import Flask, request
-from scapy.all import *
-#from celery import Celery
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from datetime import datetime
-import threading
-import os
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config["CELERY_BROKER_URL"] = "redis://localhost:6379/0"
-#app.config["CELERY_RESULT_BACKEND"] = "redis://localhost:6379/0"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:urnotyelping00@34.22.93.25:3306/yournet"
+db = SQLAlchemy(app)
 
-# Celery 객체 생성
-#celery = Celery(app.name, broker=app.config["CELERY_BROKER_URL"], backend=app.config["CELERY_RESULT_BACKEND"])
+class Users(db.Model):
+    __tablename__ = "users"
 
-# MySQL Database 설정
-username = str(os.environ.get("MYSQL_ID_YOURNET"))
-password = str(os.environ.get("MYSQL_PASSWORD_YOURNET"))
-engine = create_engine("mysql://"+username+":"+password+"@us-cdbr-east-06.cleardb.net:3306/heroku_ec04377ff1e0856")
-Base = declarative_base()
+    user_id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    #role_flag = db.Column(db.Enum("ADMIN", "USER"), nullable=True)
+    user_ip = db.Column(db.String(255), nullable=True)
+    registeredip = db.Column(db.String(255), nullable=True)
 
-# MySQL 테이블 모델 정의
-class PacketLog(Base):
-    __tablename__ = "packet_logs"
-    id = Column(Integer, primary_key=True)
-    ip_address = Column(String(20))
-    packet_data = Column(String(1000))
-    timestamp = Column(DateTime)
+    userScoreHashTag = db.relationship("UserScoreHashTag", backref="user")
 
-# MySQL 세션 생성
-Session = sessionmaker(bind=engine)
-session = Session()
+class UserScoreHashTag(db.Model):
+    __tablename__ = "user_score_hash_tag"
 
-# Celery 작업 정의
-#@celery.task
+    user_score_hash_tag_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    hash_tag_id = db.Column(db.Integer, db.ForeignKey("hash_tag.hash_tag_id"))
+    score = db.Column(db.Integer)
+    count = db.Column(db.Integer)
 
-def process_packet(packet_data, ip_address, timestamp):
-    # 패킷 데이터 처리 및 MySQL에 저장
-    log = PacketLog(ip_address=ip_address, packet_data=packet_data, timestamp=timestamp)
-    session.add(log)
-    session.commit()
+    hashTag = db.relationship("HashTag", backref="userScoreHashtag")
 
-@app.route("/", methods=["POST"])
-def capture_packet():
-    packet_data = request.data.decode("utf-8")
-    ip_address = request.remote_addr
-    timestamp = datetime.now()
+class HashTag(db.Model):
+    __tablename__ = "hash_tag"
 
-    # Celery 작업 실행
-    process_packet.delay(packet_data, ip_address, timestamp)
+    hash_tag_id = db.Column(db.Integer, primary_key=True)
+    hash_tag_name = db.Column(db.String(255))
 
-    return "Packet captured and logged asynchronously!"
+@app.route("/users/<int:user_id>/scores", methods=["GET"])
+def get_user_scores(user_id):
+    user = Users.query.get(user_id)
+    if user is None:
+        return jsonify({"error": "User not found"}), 404
+
+    scores = {}
+    for score in user.userScoreHashTag:
+        hash_tag_name = score.hashTag.hash_tag_name
+        scores[hash_tag_name] = score.score
+
+    return jsonify(scores)
 
 if __name__ == "__main__":
-    app_thread = threading.Thread(target=process_packet)
-    app_thread.start()
-    app.run(debug=True)
+    app.run()
